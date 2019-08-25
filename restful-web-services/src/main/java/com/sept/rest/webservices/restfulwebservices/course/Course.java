4979a689f01be47b0a9a80f84e7b075d63fa8125
GrimArbre <andohadi@gmail.com>
2019-08-26 05:33:09 +1000

    course bean added

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/course/Course.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/course/Course.java
@@ -1,4 +1,48 @@
 package com.sept.rest.webservices.restfulwebservices.course;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String code;
+    private Status status;
+
+    public enum Status {
+        available, enrolled, full, unavailable
+    }
+
+    //constructor methods
+    protected Course(){
+    }
+
+    public Course(long id, String name, String code, Status status){
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.status = status;
+    }
+
+    //getter setter methods
+    public long getId() { return id; }
+
+    public void setId(long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getCode() { return code; }
+
+    public void setCode(String code) { this.code = code; }
+
+    public Status getStatus() { return status; }
+
+    public void setStatus(Status status) { this.status = status; }
 }
