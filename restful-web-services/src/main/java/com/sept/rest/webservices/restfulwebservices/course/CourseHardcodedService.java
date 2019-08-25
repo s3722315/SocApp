b0dcbaeddbfed388e0e01300e95d79dfdd146e28
GrimArbre <andohadi@gmail.com>
2019-08-26 09:57:42 +1000

    added course service, resource, and JPA repository

---
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/course/CourseHardcodedService.java
@@ -0,0 +1,35 @@
+package com.sept.rest.webservices.restfulwebservices.course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseHardcodedService {
+
+    private static List<Course> courses = new ArrayList<>();
+    private static long idCounter = 0;
+
+    static {
+        courses.add(new Course(++idCounter, "Programming 1", "EEET0001", Course.Status.available));
+        courses.add(new Course(++idCounter, "Web Programming", "EEET0002", Course.Status.available));
+        courses.add(new Course(++idCounter, "Software Fundamentals", "COSC0034", Course.Status.full));
+    }
+
+    public List<Course> findAll() { return courses; }
+
+    public Course.Status checkStatus(Course course){
+        return course.getStatus();
+    }
+
+    public Course findByID(long id) {
+        for (Course course : courses) {
+            if (course.getId() == id) {
+                return course;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/course/CourseJpaRepository.java
@@ -0,0 +1,11 @@
+package com.sept.rest.webservices.restfulwebservices.course;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseJpaRepository extends JpaRepository<Course, Long> {
+    List<Course> findByName(String name);
+}
