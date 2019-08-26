f3e3dbd8c797e4414a2211fc8e18bed3159c52e3
Andy Pham <s3722315@student.rmit.edu.au>
2019-08-26 10:51:12 +1000

    fixing classpath not able to change branch

---
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/CourseServiceTests.java
@@ -0,0 +1,16 @@
+package com.sept.rest.webservices.restfulwebservices;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CourseServiceTests
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
