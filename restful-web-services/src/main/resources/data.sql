bbca60cc809652839398583d99538f0f39f1c378
GrimArbre <andohadi@gmail.com>
2019-09-12 09:35:01 +1000

    moved all restful services to JPA database

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -7,3 +7,17 @@ values(10002, 'sept', 'Learn Data JPA', sysdate(), false);
 insert into todo(id, username,description,target_date,is_done)
 values(10003, 'sept', 'Learn Microservices', sysdate(), false);
 
+insert into course(id, username, coursename, code, status)
+values (1, 'sept', 'Programming 1', 'EEET0001', 'available');
+
+insert into course(id, username, coursename, code, status)
+values (2, 'sept', 'Web Programming', 'EEET0002', 'available');
+
+insert into course(id, username, coursename, code, status)
+values (3, 'sept', 'Software Fundamentals', 'COSC0034', 'full');
+
+insert into course(id, username, coursename, code, status)
+values (4, 'sept', 'Software Testing', 'COSC0035', 'unavailable');
+
+insert into course(id, username, coursename, code, status)
+values (5, 'sept', 'Capstone Project', 'EEET0003', 'enrolled');
