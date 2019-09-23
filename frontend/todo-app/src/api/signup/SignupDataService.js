912e9adffe35af8804c9592d0e57ed84a754b9ba
Andy Pham <s3722315@student.rmit.edu.au>
2019-09-23 14:09:52 +1000

    quick fix temp Signupdataservice

--- frontend/todo-app/src/api/signup/SignupDataService.js
+++ frontend/todo-app/src/api/signup/SignupDataService.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 import { API_URL, JPA_API_URL } from '../../Constants'
 
 class SignupDataService {
-    createNewAccount() {
-        return axios.put(`${JPA_API_URL}/blah)
+    createNewAccount(account) {
+        return axios.put(`${JPA_API_URL}/blah`, account)
     }
 }
 
