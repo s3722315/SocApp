49d3c3a507f7ad2d52a5e8eff7a5a10ab4f5adb6
GrimArbre <andohadi@gmail.com>
2019-09-30 11:39:26 +1000

    set up course test file

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/CourseJpaResourceTest.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/CourseJpaResourceTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -18,35 +20,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RestfulWebServicesApplicationTests {
+public class CourseJpaResourceTest {
 
-	CourseJpaRepository courseJpaRepository;
-	CourseJpaResource courseJpaResource;
+    @Autowired
+    CourseJpaRepository courseJpaRepository;
+
+    @Autowired
+    CourseJpaResource courseJpaResource;
 
 
-	@BeforeAll
-	static void init() {
+    @BeforeAll
+    static void init() {
 
-	}
+    }
 
-	@AfterAll
-	static void finish() {
+    @AfterAll
+    static void finish() {
 
-	}
+    }
 
-	@BeforeEach
-	void setUp() {}
+    @BeforeEach
+    void setUp() {}
 
-	@AfterEach
-	void tearDown() {}
+    @AfterEach
+    void tearDown() {}
 
-	@Test
-	public void test(){
-		Course course = new Course(1, "sept", "Programming 1", "EEET01", "enrolled");
-		courseJpaRepository.save(course);
-		List<Course> list = courseJpaResource.getMyCourses("sept");
+    @Test
+    public void test(){
+        Course course = new Course(1, "sept", "Programming 1", "EEET01", "enrolled");
+        courseJpaRepository.save(course);
+        List<Course> list = courseJpaResource.getMyCourses("sept");
 
-		assertEquals(1, 1);
-	}
+        assertEquals(1, 1);
+    }
 
 }
