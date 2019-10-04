6fb1d7c0c6f36a35e8633a042af78fed9e172e10
Andy Pham <s3722315@student.rmit.edu.au>
2019-10-04 16:12:33 +1000

    updating local Develop

--- frontend/todo-app/src/api/signup/SignupDataService.js
+++ frontend/todo-app/src/api/signup/SignupDataService.js
@@ -3,11 +3,7 @@ import { JPA_API_URL } from '../../Constants'
 
 class SignupDataService {
     createNewAccount(username, password) {
-<<<<<<< HEAD
-        return axios.post(`${JPA_API_URL}/new-account`,{
-=======
         return axios.post(`${JPA_API_URL}/new-account`, {
->>>>>>> e68c8ccfee9b95c9728510d7449a44eb3bc8fac8
         username, 
         password
         })
